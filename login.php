b390c2965d4333027e86ff407d137b9172745b8d
Andrae Browne <brownexandrae@gmail.com>
2020-10-14 15:32:07 -0400

    Skeleton + Page 1

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php include 'header.php'; ?>
+
+<!-- Call to Action -->
+<section class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Login Page</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php'; ?>
