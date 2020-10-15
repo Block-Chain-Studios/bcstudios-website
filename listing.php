2550d48c5c1cb45302193983d69436f31f30e8fd
Andrae Browne <brownexandrae@gmail.com>
2020-10-15 00:59:48 -0400

    new 1

--- listing.php
+++ listing.php
@@ -2,33 +2,27 @@
 
 <!-- Call to Action -->
 <section class="listing">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <img src="img/listing-bg.jpg" alt="form" class="background-img" />
+    <img src="img/listing-bg.jpg" alt="form" class="background-img" />
 
-                <div class="logo_wrapper float_right">
-                    <a href="#" class="logo-img-wrapper" target="_blank">
-                        <img src="img/listing-logo.jpg" class="logo" alt="logo" />
-                    </a>
-                </div>
+    <div class="logo_wrapper float_right">
+        <a href="#" class="logo-img-wrapper" target="_blank">
+            <img src="img/listing-logo.jpg" class="logo" alt="logo" />
+        </a>
+    </div>
 
-                <div class="popup_image">
-                    <div class="img1">
-                        <img src="img/listing-map.jpg" alt="map" />    
-                    </div>
-                    <div class="img2">
-                        <img src="img/listing-img.jpg" alt="image" />    
-                    </div>
-                </div>
-
-                <!-- The Modal -->
-                <div id="myModal" class="modal">
-                    <span class="close">&times;</span>
-                    <img class="modal-content" id="img01" />
-                </div>
-            </div>
+    <div class="popup_image">
+        <div class="img1">
+            <img src="img/listing-map.jpg" alt="map" />    
         </div>
+        <div class="img2">
+            <img src="img/listing-img.jpg" alt="image" />    
+        </div>
+    </div>
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <span class="close">&times;</span>
+        <img class="modal-content" id="img01" />
     </div>
 </section>
 
